6804cf39c29165172ac23903cd4c9776297d2560
唐都 <7277710@qq.com>
2018-12-14 11:23:42 +0800

    1.添加事件修饰应用 2.0.0.3

---
+++ 3.event descripe like prevent like using on <a>/l3.js
@@ -0,0 +1,27 @@
+new Vue({
+	el:"#l3",
+	data:{
+		age:30,
+		x:0,
+		y:0
+	},
+	methods:{
+		add:function() {
+			this.age++;
+		},
+
+		minus:function(){
+			this.age--;
+		},
+		updateXY:function(event){
+			console.log(event);
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		},
+
+		alert:function(){
+			alert("test show");
+		}
+	}
+
+});
