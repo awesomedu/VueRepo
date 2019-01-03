af2e9826915e0079695679faee262997f1d5bd0c
唐都 <7277710@qq.com>
2019-01-03 09:43:14 +0800

    1.axios 的使用，见request.vue

---
+++ 14.axios/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    {{msg}}
+    <v-request></v-request>
+    
+  </div>
+</template>
+
+<script>
+import Home from './comp/Home.vue'
+import News from './comp/News.vue'
+import Request from './comp/Request'
+ 
+
+export default {
+  data(){
+    return {
+      msg:'app 头部'
+    }
+  },
+  components:{
+    'v-home':Home,
+    'v-news':News,
+    'v-request':Request
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align:center;
+  color:red;
+}
+</style>
---
+++ 14.axios/src/comp/Request.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <h2>{{msg}}</h2>
+        <button @click="request">click me</button>
+         <ul>
+        <li v-for="item in list">
+            {{item.title}}
+        </li>
+    </ul>
+    </div>
+</template>
+
+
+<script>
+import Axios from 'axios'
+    export default{
+        data(){
+            return {
+                msg : "请求组件",
+                list:[]
+            }
+        },
+        methods:{
+            request(){
+                var api = 'http://www.phonegap100.com/appapi.php?a=getPortalList&catid=20&page=1'
+                Axios.get(api).then((response)=>{
+                    console.log(response);
+                    this.list = response.data.result;
+                }).catch((error)=>{
+                    console.log(error);
+                })       
+            }
+        }
+    }
+
+</script>
+
+
+<style scoped>
+    h2{
+        background:brown;
+        text-align:center;
+    }
+
+    ul{
+        list-style:none;
+        margin:0px;
+        padding:0px;
+        text-align:left;
+    }
+
+</style>
